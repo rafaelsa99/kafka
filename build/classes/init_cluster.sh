2e9afc68b7031930d412eb6cba8d8d943cba60ef
rafaelsa99 <rafa.sa_@hotmail.com>
2021-05-24 10:51:26 +0100

    reverse commit

---
+++ build/classes/init_cluster.sh
@@ -0,0 +1,8 @@
+# scripts to initialize the Kafka Cluster
+gnome-terminal -- ./zookeeper.sh
+gnome-terminal -- ./kafka_server1.sh
+gnome-terminal -- ./kafka_server2.sh
+gnome-terminal -- ./kafka_server3.sh
+gnome-terminal -- ./kafka_server4.sh
+gnome-terminal -- ./kafka_server5.sh
+gnome-terminal -- ./kafka_server6.sh
